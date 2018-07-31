50ba5b28247b3c919713d5482c6dd6a928213b37
David Kyalo <davidmkyalo@gmail.com>
2018-07-31 19:50:00 +0300

    update

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-import setuptools
+from setuptools import setup, find_packages_ns
 
 
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 
-setuptools.setup(
-	name="flex_ussd",
-	version="0.0.1",
+setup(
+	name="Flex-Ussd",
+	version="0.0.0dev1",
 	author="David Kyalo",
 	author_email="davidmkyalo@gmail.com",
-	description="A USSD application framework",
+	description="USSD application framework",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/pypa/sampleproject",
-	packages=setuptools.find_packages(),
+	packages=find_packages_ns(),
 	classifiers=(
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
